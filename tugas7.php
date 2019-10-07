aaf2f9d3d85dc505e7700acb3c08fc5a3b8e0e9d
yohanes_prima <jooprima@gmail.com>
2019-10-07 10:51:13 +0700

    Tugas 7 PHP
    tugas ini dikumpukan untuk niomic

---
+++ tugas7.php
@@ -0,0 +1,131 @@
+<?php
+$angka = [1,2,3,4,5,6,7,8,9,10];
+
+?>
+<h1>Tabel Bilangan</h1>
+<table border="1">
+  <tr>
+         <td>Bilangan</td>
+         <td>Jenis</td>
+  </tr>
+  <tr>
+        <td><?php echo "$angka[0]" ?></td>
+        <td>
+          <?php
+          //Tulis jawaban disini
+          if($angka[0] % 2 == 0)
+                {echo "Bilangan Genap";}
+            else
+                {echo "Bilangan Ganjil";}
+        ?>
+        </td>
+  </tr>
+  <tr>
+        <td><?php echo "$angka[1]" ?></td>
+        <td>
+          <?php
+          //Tulis jawaban disini
+          if($angka[1] % 2 == 0)
+                {echo "Bilangan Genap";}
+            else
+                {echo "Bilangan Ganjil";}
+          ?>
+        </td>
+  </tr>
+  <tr>
+        <td><?php echo "$angka[2]" ?></td>
+        <td>
+          <?php
+          //Tulis jawaban disini
+          if($angka[2] % 2 == 0)
+                {echo "Bilangan Genap";}
+            else
+                {echo "Bilangan Ganjil";}
+          ?>
+        </td>
+  </tr>
+  <tr>
+ <td><?php echo "$angka[3]" ?></td>
+ <td>
+        <?php
+        //Tulis jawaban disini
+        if($angka[3] % 2 == 0)
+              {echo "Bilangan Genap";}
+          else
+              {echo "Bilangan Ganjil";}
+        ?>
+ </td>
+</tr>
+<tr>
+ <td><?php echo "$angka[4]" ?></td>
+ <td>
+        <?php
+        //Tulis jawaban disini
+        if($angka[4] % 2 == 0)
+              {echo "Bilangan Genap";}
+          else
+              {echo "Bilangan Ganjil";}
+        ?>
+ </td>
+</tr>
+<tr>
+ <td><?php echo "$angka[5]" ?></td>
+ <td>
+        <?php
+        //Tulis jawaban disini
+        if($angka[5] % 2 == 0)
+              {echo "Bilangan Genap";}
+          else
+              {echo "Bilangan Ganjil";}
+        ?>
+ </td>
+</tr>
+<tr>
+ <td><?php echo "$angka[6]" ?></td>
+ <td>
+        <?php
+        //Tulis jawaban disini
+        if($angka[6] % 2 == 0)
+              {echo "Bilangan Genap";}
+          else
+              {echo "Bilangan Ganjil";}
+        ?>
+ </td>
+</tr>
+<tr>
+ <td><?php echo "$angka[7]" ?></td>
+ <td>
+        <?php
+        //Tulis jawaban disini
+        if($angka[7] % 2 == 0)
+              {echo "Bilangan Genap";}
+          else
+              {echo "Bilangan Ganjil";}
+        ?>
+ </td>
+</tr>
+<tr>
+ <td><?php echo "$angka[8]" ?></td>
+ <td>
+        <?php
+        //Tulis jawaban disini
+        if($angka[8] % 2 == 0)
+              {echo "Bilangan Genap";}
+          else
+              {echo "Bilangan Ganjil";}
+        ?>
+ </td>
+</tr>
+<tr>
+ <td><?php echo "$angka[9]" ?></td>
+ <td>
+        <?php
+        //Tulis jawaban disini
+        if($angka[9] % 2 == 0)
+              {echo "Bilangan Genap";}
+          else
+              {echo "Bilangan Ganjil";}
+        ?>
+ </td>
+</tr>
+</table>
